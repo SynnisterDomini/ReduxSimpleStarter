c8618f8d8b930566c675d13cb7934e77f270c1b7
isaac <message.isaac.lee@gmail.com>
2017-11-16 21:00:31 -0800

    working on tutorial, trying to set up actions

---
+++ src/containers/book-list.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
+
+class BookList extends Component {
+renderList() {
+    return this.props.books.map((book) => {
+        return (
+            <li 
+            key={book.title}
+            onclick={() => this.props.selectBook(book)} 
+            className="list-group-item">
+            {book.title}
+            </li>
+        )
+    })
+}
+
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    // forces a rerender if state changes
+    // whatever is returned will become props of book-list
+    return {
+        books: state.books
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ selectBook: selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'die alone'},
+        {title: 'sadness'},
+        {title: 'death and destruction'},        
+        {title: 'true boredom'}
+    ]
+}
